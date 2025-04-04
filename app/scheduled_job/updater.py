801448d901d9323f949bc9209240c106c633f927
xiidot1303 <xii1303@inbox.ru>
2025-04-04 09:11:13 +0500

    update apscheduler jobs

--- app/scheduled_job/updater.py
+++ app/scheduled_job/updater.py
@@ -2,9 +2,15 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from django_apscheduler.jobstores import register_events, DjangoJobStore
 from app.scheduled_job import *
 from asgiref.sync import async_to_sync
+from app.scheduled_job.bitrix_job import *
 
 class jobs:
     scheduler = BackgroundScheduler(timezone='Asia/Tashkent')
     scheduler.add_jobstore(DjangoJobStore(), 'djangojobstore')
     register_events(scheduler)
-    # scheduler.add_job(, 'interval', minutes=5)
+    scheduler.add_job(
+        async_to_sync(fetch_and_create_stores), 'interval', minutes=300)
+    scheduler.add_job(
+        async_to_sync(fetch_and_create_products), 'interval', minutes=20)
+    scheduler.add_job(
+        async_to_sync(fetch_and_create_products), 'interval', minutes=5)
